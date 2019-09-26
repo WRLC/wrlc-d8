5023f462f192c993556ffb73f7be07bf36d11ba0
Tom Boone <boone@wrlc.org>
2019-09-26 12:09:49 -0400

    Remove db creds from settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -280,7 +280,7 @@ $config_directories = [];
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = 'XeWWEupl7cYeRDHDmJWWdxamX5_4KsAaLPROsiuTkUB9rU3T2WLxIsip_QYiV7axZ2puCRNLoQ';
+$settings['hash_salt'] = '';
 
 /**
  * Deployment identifier.
@@ -774,17 +774,6 @@ $settings['entity_update_backup'] = TRUE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
-$config_directories['sync'] = '../config/sync';
-$databases['default']['default'] = array (
-  'database' => 'drupal8',
-  'username' => 'drupal8',
-  'password' => 'drupal8',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
