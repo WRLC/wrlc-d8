2671dba0680f90c1690fea5bc1868aa38682ac75
Tom Boone <boone@wrlc.org>
2021-07-07 15:49:50 -0400

    update office_hours

--- web/modules/contrib/office_hours/tests/src/Kernel/OfficeHoursItemTest.php
+++ web/modules/contrib/office_hours/tests/src/Kernel/OfficeHoursItemTest.php
@@ -5,19 +5,20 @@ namespace Drupal\Tests\office_hours\Kernel;
 use Drupal\Core\Entity\Entity\EntityViewDisplay;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\entity_test\Entity\EntityTest;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\Tests\field\Kernel\FieldKernelTestBase;
 
 /**
- * Class OfficeHoursFieldTest.
+ * Class that tests OfficeHoursField.
  *
  * @package Drupal\Tests\office_hours\Kernel
  *
  * @group office_hours
  */
-class OfficeHoursFieldTest extends FieldKernelTestBase {
+class OfficeHoursItemTest extends FieldKernelTestBase {
 
   /**
    * A field storage to use in this test class.
@@ -36,12 +37,12 @@ class OfficeHoursFieldTest extends FieldKernelTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = ['office_hours'];
+  protected static $modules = ['office_hours'];
 
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
 
     // Create a field with settings to validate.
@@ -49,18 +50,33 @@ class OfficeHoursFieldTest extends FieldKernelTestBase {
       'field_name' => 'field_office_hours',
       'type' => 'office_hours',
       'entity_type' => 'entity_test',
-      'settings' => ['element_type' => 'office_hours_datelist'],
+      'cardinality' => FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED,
+      'settings' => [
+        'element_type' => 'office_hours_datelist',
+      ],
     ]);
     $this->fieldStorage->save();
+
     $this->field = FieldConfig::create([
       'field_storage' => $this->fieldStorage,
       'bundle' => 'entity_test',
-      'settings' => [],
+      'settings' => [
+        // @todo Test all settings.
+        'cardinality_per_day' => 2,
+        // 'time_format' => 'G',
+        // 'increment' => 30,
+        // 'comment' => 2,
+        // 'valhrs' => false,
+        // 'required_start' => false,
+        // 'required_end' => false,
+        // 'limit_start' => '',
+        // 'limit_end' => '',
+      ],
       'default_value' => [
         [
           'day' => 0,
-          'starthours' => 700,
-          'endhours' => 1800,
+          'starthours' => 900,
+          'endhours' => 1730,
           'comment' => 'Test comment',
         ],
         [
@@ -73,7 +89,7 @@ class OfficeHoursFieldTest extends FieldKernelTestBase {
     ]);
     $this->field->save();
 
-    /** @var @ $entity_display */
+    /** @var \Drupal\Core\Entity\Entity\EntityViewDisplay $entity_display */
     $entity_display = EntityViewDisplay::create([
       'targetEntityType' => $this->field->getTargetEntityTypeId(),
       'bundle' => $this->field->getTargetBundle(),
@@ -99,20 +115,56 @@ class OfficeHoursFieldTest extends FieldKernelTestBase {
     // Verify entity creation.
     /** @var \Drupal\entity_test\Entity\EntityTest $entity */
     $entity = EntityTest::create();
-    $office_hours = [
-      'day' => 1,
-      'starthours' => 630,
-      'endhours' => 2200,
+    $field_name = 'field_office_hours';
+    $value = [
+      [
+        'day' => '2',
+        'starthours' => '1330',
+        'endhours' => '2000',
+        'comment' => ''],
+      [
+        'day' => '3',
+        'starthours' => '900',
+        'endhours' => '2000',
+        'comment' => ''],
     ];
-    $entity->set('field_office_hours', $office_hours);
+    $entity->set($field_name, $value);
     $entity->setName($this->randomMachineName());
     $this->entityValidateAndSave($entity);
 
     // Verify entity has been created properly.
     $id = $entity->id();
     $entity = EntityTest::load($id);
-    $this->assertInstanceOf(FieldItemListInterface::class, $entity->get('field_office_hours'));
-    $this->assertInstanceOf(FieldItemInterface::class, $entity->get('field_office_hours')->first());
+    $this->assertInstanceOf(FieldItemListInterface::class, $entity->get($field_name));
+    $this->assertInstanceOf(FieldItemInterface::class, $entity->get($field_name)->first());
+
+    // Verify changing the field value.
+    $new_value = [
+      [
+        'day' => '2',
+        'starthours' => '1430',
+        'endhours' => '2000',
+        'comment' => ''],
+      [
+        'day' => '3',
+        'starthours' => '1900',
+        'endhours' => '2000',
+        'comment' => ''],
+    ];
+    // $entity->$field_name->value = $new_value;
+    $entity->$field_name->setValue($new_value);
+    $test_value = $entity->$field_name->first()->getValue();
+
+    // Read changed entity and assert changed values.
+    $this->entityValidateAndSave($entity);
+    $entity = EntityTest::load($id);
+    $test_value = $entity->$field_name->first()->getValue();
+    $this->assertEquals(implode('/',$new_value[0]), implode('/',$test_value));
+
+    // Test the generateSampleValue() method.
+    $entity = EntityTest::create();
+    $entity->$field_name->generateSampleItems();
+    $this->entityValidateAndSave($entity);
   }
 
 }
